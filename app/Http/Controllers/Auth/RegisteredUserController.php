f9089edf4386d402ed57cbbfc8dd4dc8dff073a4
Yoga <muliayoga8@gmail.com>
2025-08-06 16:34:37 +0700

    update

--- app/Http/Controllers/Auth/RegisteredUserController.php
+++ app/Http/Controllers/Auth/RegisteredUserController.php
@@ -42,7 +42,7 @@ class RegisteredUserController extends Controller
         ]);
 
         event(new Registered($user));
-
+        $user->assignRole('Pimpinan');
         Auth::login($user);
 
         return redirect(route('dashboard', absolute: false));
